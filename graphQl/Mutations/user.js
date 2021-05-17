afbd5bf7fb648d4f70ec9d528cd7a94c9a926b83
Phil Haines <phil.haines92@gmail.com>
2021-05-17 08:55:39 -0600

    signIn mutation extracted

--- graphQl/Mutations/user.js
+++ graphQl/Mutations/user.js
@@ -24,4 +24,26 @@ module.exports = {
 			token: getToken(user),
 		};
 	},
+
+	signIn: async (_, { input }, { database }) => {
+		const user = await database
+			.collection("Users")
+			.findOne({ email: input.email });
+		if (!user) {
+			throw new Error("Invalid Credentials");
+		} else {
+			const isPasswordCorrect = bcrypt.compareSync(
+				input.password,
+				user.password
+			);
+			if (!isPasswordCorrect) {
+				throw new Error("Invalid Credentials");
+			} else {
+				return {
+					user,
+					token: getToken(user),
+				};
+			}
+		}
+	},
 };
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const typeDefs = require("../graphQl/typeDefs");
-const { signUp } = require("../graphQl/Mutations/user");
+const { signUp, signIn } = require("../graphQl/Mutations/user");
 const { addUserToRestaurant } = require("../graphQl/Mutations/restaurant");
 const {
 	myRestaurants,
@@ -47,27 +47,7 @@ const resolvers = {
 
 	Mutation: {
 		signUp,
-		signIn: async (_, { input }, { database }) => {
-			const user = await database
-				.collection("Users")
-				.findOne({ email: input.email });
-			if (!user) {
-				throw new Error("Invalid Credentials");
-			} else {
-				const isPasswordCorrect = bcrypt.compareSync(
-					input.password,
-					user.password
-				);
-				if (!isPasswordCorrect) {
-					throw new Error("Invalid Credentials");
-				} else {
-					return {
-						user,
-						token: getToken(user),
-					};
-				}
-			}
-		},
+		signIn,
 
 		createRestaurant: async (_, { title }, { database, user }) => {
 			if (!user) {
